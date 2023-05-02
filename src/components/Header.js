85081b210b9cc358eb5ceeb2a3b670b9618baefa
Tia4130 <Tiaakiki12@gmail.com>
2023-05-02 12:11:44 +0300

    fixing test github error

--- src/components/Header.js
+++ src/components/Header.js
@@ -39,7 +39,7 @@ export default function Header() {
                     <span className='mx-4 home-tab'><Link to="/about">About</Link></span>
                     <span className='mx-4 home-tab'><Link to="/projects">Projects</Link></span>
                     <span className='mx-4 home-tab'><Link to="/contact-me"> Contact</Link></span>
-                    <div>test</div>
+                    <div>test1</div>
 
                     <img id="toggle-btn" className='home-img-logo' src={logoUser} alt="logo" />
                 </div>
