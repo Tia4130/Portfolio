d94eb11c2f82ec23c430362882014c8b65eedc93
Tia4130 <Tiaakiki12@gmail.com>
2023-06-06 22:07:42 +0300

    adding toogle night light mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import React, { useState } from 'react'
+import { createContext } from 'react'
+import ReactSwitch from 'react-switch';
+
 
 //COMPONENTS
 import Home from './components/Home';
@@ -7,17 +11,32 @@ import Contactme from './components/Contactme';
 import Projects from './components/Projects';
 
 import { Route, Routes } from "react-router-dom"
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+  const toogleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  }
   return (
-    <div className="App flex-column">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact-me" element={<Contactme />} />
-        {/* <Route path="/projects" element={<Projects />} /> */}
-      </Routes>
-    </div>
+    <ThemeContext.Provider value={{ theme, toogleTheme }}>
+      <div className="App flex-column" id={theme}>
+        <div className='switch'>
+          <label>{theme === "light" ? "light mode" : "dark mode"}</label>
+          <ReactSwitch
+            onChange={toogleTheme}
+            checked={theme === "light"}
+          />
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact-me" element={<Contactme />} />
+          {/* <Route path="/projects" element={<Projects />} /> */}
+        </Routes>
+
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
